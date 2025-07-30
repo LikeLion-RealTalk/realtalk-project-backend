ed908248269752d03d063d0031d3b9bde9e12bc3
june <0721hjp@naver.com>
2025-07-31 01:36:52 +0900

    init: 프로젝트 초기 세팅 - 프로젝트 기본 구조 설정 - config 파일 생성(swagger, security) - 공통 에러 응답 객체 생성 - 예외처리 관련 클래스 생성(CustomException, ErrorCode, GlobalExceptionHandler) - application.yml mysql 기본 설정

---
+++ src/main/java/com/likelion/realtalk/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.likelion.realtalk.global.exception;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+
+}
---
+++ src/main/java/com/likelion/realtalk/infra/claude/ClaudeAiClient.java
@@ -0,0 +1,4 @@
+package com.likelion.realtalk.infra.claude;
+
+public class ClaudeAiClient {
+}
