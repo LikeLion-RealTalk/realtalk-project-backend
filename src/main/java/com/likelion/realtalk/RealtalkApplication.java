bed3ba7b7708a11d234306bff1ea55a391c3e580
june <0721hjp@naver.com>
2025-07-30 15:07:04 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/likelion/realtalk/RealtalkApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.realtalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtalkApplication.class, args);
+	}
+
+}
