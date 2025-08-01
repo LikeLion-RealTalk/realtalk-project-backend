1f1570b70fe471b4e5236f4b19aaa30713f59767
Park Sang Min <sm0104224@gmail.com>
2025-08-01 15:31:28 +0900

    feat: debate 구조 설계

---
+++ src/main/java/com/likelion/realtalk/debate/service/DebateEventPublisher.java
@@ -0,0 +1,30 @@
+package com.example.demo.service;
+
+import com.example.demo.model.DebateRoom;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DebateEventPublisher {
+
+    private final ApplicationEventPublisher eventPublisher;
+
+    public void publishDebateStart(DebateRoom room) {
+        eventPublisher.publishEvent(new DebateStartEvent(this, room));
+    }
+
+    public static class DebateStartEvent extends org.springframework.context.ApplicationEvent {
+        private final DebateRoom room;
+
+        public DebateStartEvent(Object source, DebateRoom room) {
+            super(source);
+            this.room = room;
+        }
+
+        public DebateRoom getRoom() {
+            return room;
+        }
+    }
+}
