c4750fcf1002a82760fe444fc7d00ecd48a3979b
Park Sang Min <sm0104224@gmail.com>
2025-08-04 17:33:49 +0900

    refactor: api endpoint update & index.html upgrade

--- src/main/java/com/likelion/realtalk/debate/controller/DebateController.java
+++ src/main/java/com/likelion/realtalk/debate/controller/DebateController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.likelion.realtalk.debate.dto.ChatMessage;
@@ -23,6 +24,7 @@ import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/api/debate-rooms")
 public class DebateController {
 
     private final SimpMessageSendingOperations messagingTemplate;
@@ -45,13 +47,13 @@ public class DebateController {
         return ResponseEntity.ok(room);
     }
 
-    @GetMapping("/debate/rooms")
+    @GetMapping("/all")
     @ResponseBody
     public ResponseEntity<List<DebateRoomResponse>> getAllRooms() {
         return ResponseEntity.ok(debateService.findAllRooms());
     }
 
-    @GetMapping("/api/debate-rooms/summary/{roomId}")
+    @GetMapping("/{roomId}")
     @ResponseBody
     public ResponseEntity<DebateRoomResponse> getRoomSummary(@PathVariable Long roomId) {
         DebateRoomResponse response = debateService.findRoomSummaryById(roomId);
