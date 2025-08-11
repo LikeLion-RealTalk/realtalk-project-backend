dc2a6de2b213ff36bab172862328b4d64df33a36
Park Sang Min <sm0104224@gmail.com>
2025-08-11 20:21:44 +0900

    fix: WebSocketConfig.java 충돌 fix

--- src/main/java/com/likelion/realtalk/global/config/WebSocketConfig.java
+++ src/main/java/com/likelion/realtalk/global/config/WebSocketConfig.java
@@ -17,7 +17,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   @Override
   public void configureMessageBroker(MessageBrokerRegistry registry) {
     /** 서버가 보내는 메시지를 클라이언트가 구독할 때 사용하는 경로 **/
-    registry.enableSimpleBroker("/topic"); // 구독용 경로 서버 -> 클라이언트
+    registry.enableSimpleBroker("/topic", "/sub:"); // 구독용 경로 서버 -> 클라이언트
 
     /**클라이언트가 서버에 메시지를 보낼 때 사용하는 경로 접두어   ->   @MessageMapping **/
     registry.setApplicationDestinationPrefixes("/pub"); //  클라이언트 ->  서버
@@ -27,13 +27,16 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     // 토론방 연결 endpoint
     registry.addEndpoint("/ws-debate")
-        .setAllowedOriginPatterns("*") // TODO. 추후 변경
+        .setAllowedOriginPatterns("*") // TODO: 운영 시 구체 도메인으로 제한
+        .withSockJS();
+
+    registry.addEndpoint("/ws-speech")
+        .setAllowedOriginPatterns("*")
+        .withSockJS();
+
+    registry.addEndpoint("/ws-stomp")
+        .setAllowedOriginPatterns("*")
         .withSockJS();
 
   }
-
-
-
-
-
 }
