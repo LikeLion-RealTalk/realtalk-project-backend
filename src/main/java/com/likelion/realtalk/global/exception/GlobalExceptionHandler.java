b967c2659e10098ca7917aada96e76d16ca0f7c3
Park Sang Min <sm0104224@gmail.com>
2025-08-11 12:06:26 +0900

    fix: GlobalExceptionHandler.java 충돌 처리

--- src/main/java/com/likelion/realtalk/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/likelion/realtalk/global/exception/GlobalExceptionHandler.java
@@ -1,15 +1,16 @@
 package com.likelion.realtalk.global.exception;
 
+
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.stream.Collectors;
 
