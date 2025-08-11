d708b317a186cc9459a318f9f522139f5dfaef09
Park Sang Min <sm0104224@gmail.com>
2025-08-11 20:28:15 +0900

    fix: merge 충돌 해결

--- src/main/java/com/likelion/realtalk/global/config/WebSocketConfig.java
+++ src/main/java/com/likelion/realtalk/global/config/WebSocketConfig.java
@@ -1,6 +1,5 @@
 package com.likelion.realtalk.global.config;
 
-import com.likelion.realtalk.domain.webrtc.handler.SignalingHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -8,6 +7,8 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import com.likelion.realtalk.domain.webrtc.handler.SignalingHandler;
 
 @Configuration
 @EnableWebSocketMessageBroker
