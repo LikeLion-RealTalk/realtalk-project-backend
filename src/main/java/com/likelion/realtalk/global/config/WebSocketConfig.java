2891789f48ab2d03a6daf21a42baa649a4d406e9
Park Sang Min <sm0104224@gmail.com>
2025-08-11 20:21:13 +0900

    fix: WebSocketConfig.java 충돌 잡기

--- src/main/java/com/likelion/realtalk/global/config/WebSocketConfig.java
+++ src/main/java/com/likelion/realtalk/global/config/WebSocketConfig.java
@@ -13,10 +13,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   @Override
   public void configureMessageBroker(MessageBrokerRegistry registry) {
     // 서버 -> 클라이언트 구독 경로: 두 설정 모두 유지 (/topic, /sub)
-    registry.enableSimpleBroker("/topic", "/sub");
+    /** 서버가 보내는 메시지를 클라이언트가 구독할 때 사용하는 경로 **/
+    registry.enableSimpleBroker("/topic", "/sub:"); // 구독용 경로 서버 -> 클라이언트
 
     // 클라이언트 -> 서버 전송 경로 접두어 (공통)
-    registry.setApplicationDestinationPrefixes("/pub");
+    /**클라이언트가 서버에 메시지를 보낼 때 사용하는 경로 접두어   ->   @MessageMapping **/
+    registry.setApplicationDestinationPrefixes("/pub"); //  클라이언트 ->  서버
   }
 
   @Override
