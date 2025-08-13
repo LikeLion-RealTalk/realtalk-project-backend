6bf08a522babae16bb326d6f614fe88b43f8ec4d
박세정 <parksj980613@gmail.com>
2025-08-13 10:34:24 +0900

    fix : enum 추가, Category 엔티티 추가

--- src/main/java/com/likelion/realtalk/domain/debate/service/DebateRoomService.java
+++ src/main/java/com/likelion/realtalk/domain/debate/service/DebateRoomService.java
@@ -1,5 +1,7 @@
 package com.likelion.realtalk.domain.debate.service;
 
+import com.likelion.realtalk.domain.category.entity.Category;
+import com.likelion.realtalk.domain.category.repository.CategoryRepository;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -25,6 +27,7 @@ public class DebateRoomService {
     private final DebateRoomRepository debateRoomRepository;
     private final RedisRoomTracker redisRoomTracker;
     private final RoomIdMappingService roomIdMappingService;
+    private final CategoryRepository categoryRepository;
     // private final StringRedisTemplate stringRedisTemplate; // ← 주입 추가
     // private final DebateEventPublisher debateEventPublisher;
 
@@ -45,7 +48,7 @@ public class DebateRoomService {
                 .roomId(room.getRoomId())
                 .title(room.getTitle())
                 .category(AiSummaryResponse.CategoryDto.builder()
-                        .id(room.getCategoryId())
+                        .id(room.getCategory().getId())
                         .name("카테고리 이름은 추후 조회") // 카테고리 명 로직 추가 필요
                         .build())
                 .build();
@@ -73,14 +76,14 @@ public class DebateRoomService {
 
                 Long currentSpeaker  = redisRoomTracker.getCurrentSpeakers(room.getRoomId());
                 Long currentAudience = redisRoomTracker.getCurrentAudiences(room.getRoomId());
-                Long elapsedSeconds  = calculateElapsedSeconds(room.getCreatedAt());
+                Long elapsedSeconds  = calculateElapsedSeconds(room.getStartedAt());
 
                 return DebateRoomResponse.builder()
                         .roomId(externalId)
                         .title(room.getTitle())
                         .status(room.getStatus().name())
                         .category(DebateRoomResponse.CategoryDto.builder()
-                                .id(room.getCategoryId())
+                                .id(room.getCategory().getId())
                                 .name("카테고리 이름은 추후 조회")
                                 .build())
                         .sideA(room.getSideA())
@@ -105,14 +108,14 @@ public class DebateRoomService {
 
         Long currentSpeaker  = redisRoomTracker.getCurrentSpeakers(pk);
         Long currentAudience = redisRoomTracker.getCurrentAudiences(pk);
-        Long elapsedSeconds  = calculateElapsedSeconds(room.getCreatedAt());
+        Long elapsedSeconds  = calculateElapsedSeconds(room.getStartedAt());
 
         return DebateRoomResponse.builder()
                 .roomId(roomUuid) // ★ 여기! Long이 아니라 UUID를 넣어야 함
                 .title(room.getTitle())
                 .status(room.getStatus().name())
                 .category(DebateRoomResponse.CategoryDto.builder()
-                        .id(room.getCategoryId())
+                        .id(room.getCategory().getId())
                         .name("카테고리 이름은 추후 조회")
                         .build())
                 .sideA(room.getSideA())
@@ -134,9 +137,12 @@ public class DebateRoomService {
         debateRoom.setTitle(request.getTitle()); //토론 주제
         debateRoom.setDebateDescription(request.getDebateDescription()); //토론 설명
 
+        Long categoryId = request.getCategory().getId();
+        Category category = categoryRepository.findById(categoryId).orElseThrow(() -> new IllegalArgumentException("해당 카테고리 정보를 찾을 수 없습니다."));
+
         // 카테고리 ID 추출
         if (request.getCategory() != null) {
-            debateRoom.setCategoryId(request.getCategory().getId());
+            debateRoom.setCategory(category);
         }
 
         debateRoom.setSideA(request.getSideA()); // 토론 사이드 (찬성)
@@ -148,7 +154,7 @@ public class DebateRoomService {
         debateRoom.setMaxAudience((long) request.getMaxAudience()); //최대 청중 수
 
         debateRoom.setStatus(DebateRoomStatus.waiting); // enum 값 명확히 지정
-        debateRoom.setCreatedAt(LocalDateTime.now());
+        debateRoom.setStartedAt(LocalDateTime.now());
 
         // maxParticipants는 요청에 없으면 계산해서 넣어야 할 수도 있음
         debateRoom.setMaxParticipants(
