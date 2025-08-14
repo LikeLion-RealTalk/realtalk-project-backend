927eeb4f3473855b20ff5c8361762bfa2da9bf74
Park Sang Min <sm0104224@gmail.com>
2025-08-14 10:57:52 +0900

    fix: authcontroller 다시 fix

--- src/main/java/com/likelion/realtalk/domain/auth/api/AuthController.java
+++ src/main/java/com/likelion/realtalk/domain/auth/api/AuthController.java
@@ -1,22 +1,23 @@
 package com.likelion.realtalk.domain.auth.api;
 
-import java.util.Map;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-
 import com.likelion.realtalk.domain.auth.service.AuthService;
+import com.likelion.realtalk.domain.user.dto.UserInfoDto;
 import com.likelion.realtalk.global.security.core.CustomUserDetails;
-import com.likelion.realtalk.global.security.jwt.JwtProvider;
-
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+import com.likelion.realtalk.global.security.jwt.JwtProvider;
+
 
 @RestController
 @RequestMapping("/api/auth")
@@ -30,6 +31,19 @@ public class AuthController {
       throws Exception {
     authService.reissueTokens(request, response);
     return ResponseEntity.ok().build();
+  }
+
+  // 로그인 한 사용자 정보 조회
+  @GetMapping("/me")
+  public ResponseEntity<UserInfoDto> getMyProfile(
+      @AuthenticationPrincipal CustomUserDetails userDetail) {
+    UserInfoDto userInfo = UserInfoDto.withProvider(
+        userDetail.getUserId(),
+        userDetail.getUsername(),
+        userDetail.getUser().getRole(),
+        userDetail.getUser().getAuth().getProvider()
+    );
+    return ResponseEntity.ok(userInfo);
   }
 
   // ★ 추가: access token JSON으로 발급
