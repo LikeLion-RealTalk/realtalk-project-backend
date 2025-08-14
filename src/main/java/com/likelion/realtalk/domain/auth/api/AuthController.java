cee027829df9c9bd10c262f1a3cbbfe0e42c6d79
Park Sang Min <sm0104224@gmail.com>
2025-08-14 11:02:35 +0900

    fix: for merge

--- src/main/java/com/likelion/realtalk/domain/auth/api/AuthController.java
+++ src/main/java/com/likelion/realtalk/domain/auth/api/AuthController.java
@@ -7,7 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
